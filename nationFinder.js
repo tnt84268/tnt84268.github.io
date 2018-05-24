00af58709ea576f448aaf4fdcf79d91faa813423
Danny Nguyen <dannynguyen84268@gmail.com>
2018-05-24 03:55:50 +0000

    test

---
+++ nationFinder.js
@@ -0,0 +1,10 @@
+function mainFunction() {
+    var nationsResponse = new XMLHttpRequest();
+    nationsResponse.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+    document.getElementById("changeText").innerHTML = this.responseText;
+        }
+    };
+    nationsResponse.open("GET", "https://politicsandwar.com/api/nations/", true);
+    nationsResponse.send();
+}
